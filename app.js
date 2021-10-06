b81c4a12bc5cf6aed18cad129d5d66725db1d126
Edy Segura <edysegura@gmail.com>
2021-10-06 15:56:29 -0300

    chore: fill dropdown component

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-fetch('https://countries.trevorblades.com/', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    query: `
-      query {
-        continents {
-          code
-          name
-        }
+async function fillContinentDropdown() {
+  const { continents } = await graphqlQuery(`
+    query {
+      continents {
+        code
+        name
       }
-    `
+    }
+  `)
+  const select = document.querySelector('select')
+  const createOption = (continent) => {
+    const option = new Option(continent.name, continent.code)
+    select.appendChild(option)
+  }
+  continents.forEach(createOption)
+  console.log(select)
+}
+
+async function graphqlQuery(query) {
+  const response = await fetch('https://countries.trevorblades.com/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: query })
   })
-})
-.then(response => response.json())
-.then(data => console.log(data))
+  const jsonObject = await response.json()
+  return jsonObject.data
+}
+
+fillContinentDropdown()
