847149410ffdc96b93a81834bb86cbf56dbbaa09
Edy Segura <edysegura@gmail.com>
2021-10-06 22:41:02 -0300

    chore: implement onchange event

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+function showCountries(event) {
+  const continentCode = event.target.value
+  fetchCountries(continentCode)
+}
+
+async function fetchCountries(continentCode) {
+  const { continent } = await graphqlQuery(`
+    query getCountries($code: ID!) {
+      continent(code: $code) {
+        countries {
+          name
+        }
+      }
+    }
+  `, { code: continentCode })
+  console.table(continent.countries)
+}
+
 async function fillContinentDropdown() {
   const { continents } = await graphqlQuery(`
-    query {
+    query getContinents {
       continents {
         code
         name
@@ -13,14 +31,17 @@ async function fillContinentDropdown() {
     select.appendChild(option)
   }
   continents.forEach(createOption)
-  console.log(select)
+  select.addEventListener('change', showCountries)
 }
 
-async function graphqlQuery(query) {
+async function graphqlQuery(query, variables) {
   const response = await fetch('https://countries.trevorblades.com/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: query })
+    body: JSON.stringify({
+      query: query,
+      variables: variables
+    })
   })
   const jsonObject = await response.json()
   return jsonObject.data
